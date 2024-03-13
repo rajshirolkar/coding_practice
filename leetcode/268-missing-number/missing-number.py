17e161f9b291131dcf40cc0df8279c6239df870e
rajshirolkar <35342595+rajshirolkar@users.noreply.github.com>
2024-03-12 20:58:11 -0700

    Time: 103 ms (90.21%) | Memory: 17.8 MB (77.84%) - LeetSync

--- leetcode/268-missing-number/missing-number.py
+++ leetcode/268-missing-number/missing-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        numdict = {i:1 for i in nums}
-        print(numdict)
-        for i in range(len(nums)+1):
-            if i not in numdict:
-                return i
+        N = len(nums)
+        nsum = (N * (N+1))//2
+        for i in nums:
+            nsum -= i
+        return nsum
