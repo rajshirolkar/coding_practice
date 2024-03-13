c42378189e3dfe74e5191dc6473fb59567f0634d
rajshirolkar <35342595+rajshirolkar@users.noreply.github.com>
2024-03-12 22:39:47 -0700

    Time: 802 ms (19.17%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/197-rising-temperature/rising-temperature.sql
+++ leetcode/197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,4 @@
 -- Write your PostgreSQL query statement below
 select id
 from weather w
-where recordDate > (select min(recordDate) from weather)
-    and temperature > (select temperature from weather where w.recordDate - recordDate = 1)
+where temperature > (select temperature from weather where w.recordDate - recordDate = 1)
