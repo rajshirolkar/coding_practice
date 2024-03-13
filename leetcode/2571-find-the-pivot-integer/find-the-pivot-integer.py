de9af6cf86cccdcd8cbc24f2eb9a805632df83ab
rajshirolkar <35342595+rajshirolkar@users.noreply.github.com>
2024-03-12 21:20:07 -0700

    Time: 38 ms (85.89%) | Memory: 16.4 MB (92.38%) - LeetSync

--- leetcode/2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ leetcode/2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,13 +1,13 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
         sumn = (n*(n+1))//2
-        sumdict = {0:0}
+        prev = 0
         sum1 = 0
         for i in range(1,n+1):
             sum1 += i
             # sum2 = sumn - ((i-1)*i//2)
-            sum2 = sumn - sumdict[i-1]
-            sumdict[i] = sum1
+            sum2 = sumn - prev
+            prev = sum1
             if sum1 == sum2:
                 return i
 
