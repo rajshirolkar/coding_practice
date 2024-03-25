a34e0f24ef506e316c6b447f5d3e594732fcea6c
rajshirolkar <35342595+rajshirolkar@users.noreply.github.com>
2024-03-24 22:16:57 -0700

    Time: 59 ms (5.34%) | Memory: 17 MB (5.92%) - LeetSync

---
+++ Amazon/974-reorder-data-in-log-files/reorder-data-in-log-files.py
@@ -0,0 +1,27 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        dlog, llog = [], []
+        lexdict = defaultdict(list)
+        for l in logs:
+            log = l.split(" ")
+            idf = log[0]
+            if log[1].isdigit():
+                dlog.append(l)
+            else:
+                del log[0]
+                m = " ".join(log)
+                lexdict[m].append(l)
+                print(lexdict)
+                llog.append(m)
+        llog.sort()
+        res = []
+        for s in llog:
+            if len(lexdict[s]) > 1:
+                lexdict[s].sort(key = lambda x:x[0])
+                res.append(lexdict[s][0])
+                del lexdict[s][0]
+            else:
+                res.append(lexdict[s][0])
+        for d in dlog:
+            res.append(d)
+        return res
