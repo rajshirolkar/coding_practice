d125730abd8503fb5c9c0e3b721fa875bc076bec
rajshirolkar <35342595+rajshirolkar@users.noreply.github.com>
2024-03-25 02:25:17 -0700

    Time: 78 ms (69.20%) | Memory: 19.7 MB (80.45%) - LeetSync

--- Amazon/1397-search-suggestions-system/search-suggestions-system.py
+++ Amazon/1397-search-suggestions-system/search-suggestions-system.py
@@ -1,10 +1,10 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         res = []
-        for i in range(len(searchWord)):
+        for i in range(1, len(searchWord)+1):
             temp = []
             for p in products:
-                if p.startswith(searchWord[:i+1]):
+                if p.startswith(searchWord[:i]):
                     temp.append(p)
             products = temp
             # if products: 
