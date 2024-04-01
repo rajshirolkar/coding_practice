7f19c577493153404a282767732a3a38ba57cb70
rajshirolkar <35342595+rajshirolkar@users.noreply.github.com>
2024-04-01 13:43:53 -0700

    Time: 455 ms (54.25%) | Memory: 31.3 MB (38.67%) - LeetSync

--- Amazon/1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.py
+++ Amazon/1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.py
@@ -1,19 +1,17 @@
 class Solution:
     def getMaxLen(self, nums: List[int]) -> int:
-        pos = 1 if nums[0] > 0 else 0
-        neg = 1 if nums[0] < 0 else 0
-        res = pos
-
-        for n in nums[1:]:
-            prev_pos, prev_neg = pos, neg
-            if n > 0:
-                pos += 1
-                neg = prev_neg + 1 if prev_neg > 0 else 0
-            elif n < 0:
-                neg = prev_pos + 1
-                pos = prev_neg + 1 if prev_neg > 0 else 0
-            else:
-                pos = 0
-                neg = 0
-            res = max(res,pos)
-        return res
+        n = len(nums)
+        pos, neg = 0, 0
+        if nums[0] > 0: pos = 1
+        if nums[0] < 0: neg = 1
+        ans = pos
+        for i in range(1, n):
+            if nums[i] > 0:
+                pos = 1 + pos # add to current streak of positive numbers
+                neg = 1 + neg if neg > 0 else 0 #add to current streak of negative numbers - unless no streak, then continue 0 streak
+            elif nums[i] < 0:
+                pos, neg = 1 + neg if neg > 0 else 0, 1 + pos
+            else: #when nums[i] == 0
+                pos, neg = 0, 0 #dont do anything if you already set it to 0
+            ans = max(ans, pos)
+        return ans
