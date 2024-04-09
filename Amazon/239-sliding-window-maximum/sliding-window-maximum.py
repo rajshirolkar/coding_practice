0f844c2bb4c4c256016e63d63fb7342186556d62
rajshirolkar <35342595+rajshirolkar@users.noreply.github.com>
2024-04-08 19:58:10 -0700

    Time: 1133 ms (37.03%) | Memory: 32.3 MB (67.79%) - LeetSync

---
+++ Amazon/239-sliding-window-maximum/sliding-window-maximum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        q = deque()
+        res = []
+        l, r = 0, 0
+        while r < len(nums):
+            # monotonically decreasing queue
+            # pop smaller values from q
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()   
+            q.append(r)
+
+            # remove left value from window
+            if l > q[0]:
+                q.popleft()
+
+            if (r+1) >= k:
+                res.append(nums[q[0]])
+                l += 1            
+            r += 1
+        return res
