c9be76d317c6c09c29de94520d6022a4028706bf
rajshirolkar <35342595+rajshirolkar@users.noreply.github.com>
2024-04-12 23:45:16 -0700

    Time: 435 ms (79.41%) | Memory: 19.5 MB (95.02%) - LeetSync

--- leetcode/1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ leetcode/1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,13 +1,27 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-            # 1 1 0 1 1 1 0 1 1 1
-            s = ''.join([str(i) for i in nums])
-            new_nums = s.split('0')
-            # print(new_nums)
-            if len(new_nums) == 1:
-                return len(new_nums[0]) - 1
-            max_count = 0
-            for i in range(len(new_nums) - 1):
-                max_count = max(max_count, len(new_nums[i]) + len(new_nums[i+1]))
-            return max_count
+        curr_len = prev_len = max_len = 0
+
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                max_len = max(max_len, curr_len + prev_len)
+                prev_len = curr_len
+                curr_len = 0
+            else:
+                curr_len += 1
+        if curr_len == len(nums):
+            return curr_len - 1
+        return max(max_len, curr_len + prev_len)
+
+            # INnovative
+            # # 1 1 0 1 1 1 0 1 1 1
+            # s = ''.join([str(i) for i in nums])
+            # new_nums = s.split('0')
+            # # print(new_nums)
+            # if len(new_nums) == 1:
+            #     return len(new_nums[0]) - 1
+            # max_count = 0
+            # for i in range(len(new_nums) - 1):
+            #     max_count = max(max_count, len(new_nums[i]) + len(new_nums[i+1]))
+            # return max_count
         
