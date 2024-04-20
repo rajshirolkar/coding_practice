bb99d34565b7ec15ecb5d58e2c224629de5b1969
rajshirolkar <35342595+rajshirolkar@users.noreply.github.com>
2024-04-19 23:37:00 -0700

    Time: 36 ms (82.43%) | Memory: 17 MB (34.46%) - LeetSync

--- leetcode/133-clone-graph/clone-graph.py
+++ leetcode/133-clone-graph/clone-graph.py
@@ -9,17 +9,53 @@ class Node:
 from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        new_dict = {}
-        if not node:
+        # DFS approach
+        # new_dict = {}
+        # if not node:
+        #     return None
+
+        # def dfs(node):
+        #     if node.val in new_dict:
+        #         return new_dict[node.val]
+            
+        #     copy_node = Node(node.val)
+        #     new_dict[node.val] = copy_node
+        #     for nei in node.neighbors:
+        #         copy_node.neighbors.append(dfs(nei))
+        #     return copy_node
+        # return dfs(node)
+
+        """
+        BFS Approach
+            {
+                1: node1;
+                2: node2;
+                3: node3;
+                4: node4;
+            }
+
+            1 -> 2, 4
+            2 -> 3, 1
+            3 -> 2, 4
+            4 -> 3, 1
+        """
+        if node is None:
             return None
 
-        def dfs(node):
-            if node.val in new_dict:
-                return new_dict[node.val]
-            
-            new_node = Node(node.val)
-            new_dict[node.val] = new_node
-            for nei in node.neighbors:
-                new_node.neighbors.append(dfs(nei))
-            return new_node
-        return dfs(node)
+        node_dict = {node.val: Node(node.val)}
+        start = node
+        q = deque([start])
+        while q:
+            n = q.popleft()
+            new_node = node_dict[n.val]
+
+            for nei in n.neighbors:
+                if nei.val in node_dict:
+                    new_node.neighbors.append(node_dict[nei.val])
+                else:
+                    new_nei_node = Node(nei.val)
+                    node_dict[nei.val] = new_nei_node
+                    new_node.neighbors.append(new_nei_node)
+                    q.append(nei)
+        # print(node_dict)
+        return node_dict[node.val]
