5da38d5e100b495450e94486c5fba983ef54e908
rajshirolkar <35342595+rajshirolkar@users.noreply.github.com>
2024-04-24 22:48:00 -0700

    Time: 1592 ms (32.60%) | Memory: 17.4 MB (28.89%) - LeetSync

--- leetcode/2444-longest-ideal-subsequence/longest-ideal-subsequence.py
+++ leetcode/2444-longest-ideal-subsequence/longest-ideal-subsequence.py
@@ -1,5 +1,6 @@
 class Solution:
     def longestIdealString(self, s: str, k: int) -> int:
+        # DP approach for each character in the alphabet
         dp = [0] * 26
         for c in s:
             curr = ord(c) - ord('a') # 0-indexed values
@@ -10,20 +11,17 @@ class Solution:
             dp[curr] = longest
         return max(dp)
         
-        
-        
-        n = len(s)
-        dp = {}
-        def dfs(subseq,i):
-            if i == n:
-                return len(subseq)
-            if i in dp:
-                return dp[i]
-            
-            if (not subseq) or (abs(ord(s[i]) - ord(subseq[-1])) <= k):
-                dp[i] = max(dfs(subseq, i+1), dfs(subseq+s[i], i+1))
-                return dp[i]
-            else:
-                dp[i] = dfs(subseq, i+1)
-                return dp[i]
-        return dfs("", 0)
+        # Recursive approach good for interview
+        # n = len(s)
+        # dp = {}
+        # def dfs(prev,i):
+        #     if i == n:
+        #         return 0
+        #     if (prev, i) in dp:
+        #         return dp[(prev, i)]
+        #     res = dfs(prev, i+1)
+        #     if (not prev) or (abs(ord(s[i]) - ord(prev)) <= k):
+        #         res = max(res, 1 + dfs(s[i], i+1))
+        #     dp[(prev,i)] = res
+        #     return res
+        # return dfs("", 0)
